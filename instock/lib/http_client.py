654012e41292dcfaab6190e0c1f313bb9f7bd0fa
Your Name <youremail@example.com>
2025-11-21 13:02:39 +0800

    test cookie

--- instock/lib/http_client.py
+++ instock/lib/http_client.py
@@ -7,12 +7,14 @@
 
 import requests
 from instock.core.singleton_proxy import proxys
+import time
+import random
 
 # 默认User-Agent，模拟现代浏览器
 DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
 
 # 东方财富网需要的Cookie设置
-EASTMONEY_COOKIE = "qgqp_b_id=d9603ee43a43c3fcda7a643af5b10581"
+EASTMONEY_COOKIE = "qgqp_b_id=d9603ee43a43c3fcda7a643af5b10581; st_nvi=DNgOUwl4-dConj5suyRHO6e76; st_si=07932919336390; st_pvi=06135546313648; st_sp=2025-11-21%2011%3A13%3A13; st_inirUrl=; st_sn=1; st_psi=20251121111313207-113200301321-0650867276; st_asi=delete; nid=06545c30579aca550a182fb44b85a021; nid_create_time=1763694793578; gvi=ShBFoUUT-JVAZhI_NaWfF99ec; gvi_create_time=1763694793578; fullscreengg=1; fullscreengg2=1"
 
 # 默认请求头
 DEFAULT_HEADERS = {
@@ -41,6 +43,9 @@ def get(url, **kwargs):
     :param kwargs: 其他requests参数
     :return: requests.Response对象
     """
+    # 添加随机延时，避免请求过于频繁
+    # time.sleep(random.uniform(1, 3))
+    
     # 添加默认headers（如果未提供）
     if 'headers' not in kwargs:
         kwargs['headers'] = DEFAULT_HEADERS.copy()
@@ -55,7 +60,7 @@ def get(url, **kwargs):
         proxies = proxys().get_proxies()
         if proxies:
             kwargs['proxies'] = proxies
-    
+
     # 特殊处理东方财富网API，添加必要的Cookie
     if "eastmoney.com" in url:
         if 'headers' not in kwargs:
@@ -72,6 +77,9 @@ def post(url, **kwargs):
     :param kwargs: 其他requests参数
     :return: requests.Response对象
     """
+    # 添加随机延时，避免请求过于频繁
+    # time.sleep(random.uniform(1, 3))
+    
     # 添加默认headers（如果未提供）
     if 'headers' not in kwargs:
         kwargs['headers'] = DEFAULT_HEADERS.copy()
--- test_eastmoney_cookie.py
+++ test_eastmoney_cookie.py
@@ -5,7 +5,7 @@
 测试访问东方财富网API，检查是否还需要额外的Cookie或Referer设置
 用浏览器依次访问以下URL，看看是否正常，可能需要更新cookie
 https://quote.eastmoney.com/center/gridlist.html
-http://82.push2.eastmoney.com/api/qt/clist/get?pn=1&pz=50&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=m:0%20t:6,m:0%20t:80,m:1%20t:2,m:1%20t:23,m:0%20t:81%20s:2048&fields=f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f14,f15,f16,f17,f18,f20,f21,f22,f23,f24,f25,f26,f37,f38,f39,f40,f41,f45,f46,f48,f49,f57,f61,f100,f112,f113,f114,f115,f221
+http://82.push2.eastmoney.com/api/qt/clist/get?pn=1&pz=50&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=m:0%20t:6,m:0%20t:80,m:1%20t:2,m:1%20t:23,m:0%20t:81%20s:2048&fields=f12,f14
 """
 
 import sys
@@ -19,7 +19,7 @@ def test_eastmoney_api():
     测试访问东方财富网API
     """
     # 使用东方财富网首页作为Referer
-    url = "http://82.push2.eastmoney.com/api/qt/clist/get"
+    url = "http://push2.eastmoney.com/api/qt/clist/get"
     params = {
         "pn": 1,
         "pz": 50,
@@ -30,8 +30,7 @@ def test_eastmoney_api():
         "invt": "2",
         "fid": "f12",
         "fs": "m:0 t:6,m:0 t:80,m:1 t:2,m:1 t:23,m:0 t:81 s:2048",
-        "fields": "f12,f14",
-        "_": "1623833739532",
+        "fields": "f12,f14"
     }
     
     print("正在测试访问东方财富网API...")
